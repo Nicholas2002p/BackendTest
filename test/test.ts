7e0ab917bf7cacdbdb4c6a1ae9570ab862cd175b
Nicholas Pouliezos <nick2002p@gmail.com>
2025-02-19 21:24:05 -0500

    add the beforeach and after each WIP

--- test/test.ts
+++ test/test.ts
@@ -1,9 +1,26 @@
 import { expect } from "chai";
 import sinon from "sinon";
+import { describe, it, beforeEach, afterEach } from "mocha";
 import { Context } from "../src/graphql/context";
+import { PrismaClient } from "@prisma/client";
 import {
   Mutation as MutationFromTodo,
   MutationCompletion,
   MutationTitle,
   DeleteMutation
 } from "../src/graphql/todo";
+
+describe("Create Todo", () => {
+    let prismaMock: sinon.SinonStubbedInstance<PrismaClient>;
+    let context: Context;
+
+
+    beforeEach(() => {
+    prismaMock = sinon.createStubInstance(PrismaClient);
+    context = { prisma: prismaMock } as Context;
+  });
+
+    afterEach(() => {
+    sinon.restore(); 
+  });
+});
